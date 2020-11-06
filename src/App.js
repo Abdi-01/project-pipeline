68e0f76e16497275e56eee602f72d708b5759680
hisbu <lab.hisbu@gmail.com>
2020-11-06 15:28:55 +0700

    add stage build 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Clock from 'react-live-clock';
+// import Clock from 'react-live-clock';
 
 class App extends Component {
   render() {
